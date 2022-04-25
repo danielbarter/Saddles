2f5c0a957c8fd3272af8dd9151878412f81d927d
Daniel Barter <danielbarter@gmail.com>
2022-04-25 08:35:22 -0700

    cpu and gpu nix environments

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Python library for computing saddle points built with JAX";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+
+  outputs = { self, nixpkgs }: {
+
+
+    devShells.x86_64-linux.gpu =
+      with import nixpkgs { system = "x86_64-linux"; };
+      mkShell {
+        buildInputs = with pkgs; [
+          python3
+          python3Packages.jax
+          (python3Packages.jaxlib.override { cudaSupport = true; })
+          python3Packages.matplotlib
+          nvtop
+        ];
+
+        LD_LIBRARY_PATH = with pkgs; builtins.concatStringsSep ":" [
+          # cuda shared libraries
+          "${cudatoolkit_11_2}/lib"
+          "${cudatoolkit_11_2.lib}/lib"
+
+          # nvidia driver shared libs
+          "${pkgs.linuxPackages.nvidia_x11}/lib"
+        ];
+      };
+
+
+    devShells.x86_64-linux.cpu =
+      with import nixpkgs { system = "x86_64-linux"; };
+      mkShell {
+        buildInputs = with pkgs; [
+          python3
+          python3Packages.jax
+          (python3Packages.jaxlib.override { cudaSupport = false; })
+          python3Packages.matplotlib
+        ];
+      };
+  };
+}
